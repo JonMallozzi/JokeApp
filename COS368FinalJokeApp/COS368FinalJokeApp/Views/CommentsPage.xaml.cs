a0e15be29d03b6c19d4c8f2e88f44f669f53e42e
Jon Mallozzi <Jonathan.Mallozzi@Maine.edu>
2020-05-03 00:49:10 -0400

    made search responsive kinda and cleaned up comments

--- COS368FinalJokeApp/COS368FinalJokeApp/Views/CommentsPage.xaml.cs
+++ COS368FinalJokeApp/COS368FinalJokeApp/Views/CommentsPage.xaml.cs
@@ -17,75 +17,43 @@ namespace COS368FinalJokeApp
         }
 
         private void LoadComments(string title) {
-            Comments comments = new Comments();
+            Comments commentData = new Comments();
+
+            Label[] users = {User, User1, User2, User3 ,User4 ,User5 , User6, }; 
+            Label[] comments = {Comment, Comment1, Comment2, Comment3 ,Comment4 ,Comment5 , Comment6, }; 
 
             //this would be a thousand times better with an api 
             if (title.Equals("Feed")) {
-
-                User.Text = comments.comments.Feed[0].username;
-                Comment.Text = comments.comments.Feed[0].comment;
-                User1.Text = comments.comments.Feed[1].username;
-                Comment1.Text = comments.comments.Feed[1].comment;
-                User2.Text = comments.comments.Feed[2].username;
-                Comment2.Text = comments.comments.Feed[2].comment;
-                User3.Text = comments.comments.Feed[3].username;
-                Comment3.Text = comments.comments.Feed[3].comment;
-                User4.Text = comments.comments.Feed[4].username;
-                Comment4.Text = comments.comments.Feed[4].comment;
-                User5.Text = comments.comments.Feed[5].username;
-                Comment5.Text = comments.comments.Feed[5].comment;
-                User6.Text = comments.comments.Feed[6].username;
-                Comment6.Text = comments.comments.Feed[6].comment;
-
+                int i = 0;
+                foreach (Label post in comments) {
+                    users[i].Text = commentData.comments.Feed[i].username;
+                    comments[i].Text = commentData.comments.Feed[i].comment;
+                    i++;
+                }
             }
             else if (title.Equals("Sports")) {
-                User.Text = comments.comments.Sports[0].username;
-                Comment.Text = comments.comments.Sports[0].comment;
-                User1.Text = comments.comments.Sports[1].username;
-                Comment1.Text = comments.comments.Sports[1].comment;
-                User2.Text = comments.comments.Sports[2].username;
-                Comment2.Text = comments.comments.Sports[2].comment;
-                User3.Text = comments.comments.Sports[3].username;
-                Comment3.Text = comments.comments.Sports[3].comment;
-                User4.Text = comments.comments.Sports[4].username;
-                Comment4.Text = comments.comments.Sports[4].comment;
-                User5.Text = comments.comments.Sports[5].username;
-                Comment5.Text = comments.comments.Sports[5].comment;
-                User6.Text = comments.comments.Sports[6].username;
-                Comment6.Text = comments.comments.Sports[6].comment;
-
+                int i = 0;
+                foreach (Label post in comments) {
+                    users[i].Text = commentData.comments.Sports[i].username;
+                    comments[i].Text = commentData.comments.Sports[i].comment;
+                    i++;
+                }
             }
             else if (title.Equals("Fortnite")) {
-                User.Text = comments.comments.Fortnite[0].username;
-                Comment.Text = comments.comments.Fortnite[0].comment;
-                User1.Text = comments.comments.Fortnite[1].username;
-                Comment1.Text = comments.comments.Fortnite[1].comment;
-                User2.Text = comments.comments.Fortnite[2].username;
-                Comment2.Text = comments.comments.Fortnite[2].comment;
-                User3.Text = comments.comments.Fortnite[3].username;
-                Comment3.Text = comments.comments.Fortnite[3].comment;
-                User4.Text = comments.comments.Fortnite[4].username;
-                Comment4.Text = comments.comments.Fortnite[4].comment;
-                User5.Text = comments.comments.Fortnite[5].username;
-                Comment5.Text = comments.comments.Fortnite[5].comment;
-                User6.Text = comments.comments.Fortnite[6].username;
-                Comment6.Text = comments.comments.Fortnite[6].comment;
+                int i = 0;
+                foreach (Label post in comments) {
+                    users[i].Text = commentData.comments.Fortnite[i].username;
+                    comments[i].Text = commentData.comments.Fortnite[i].comment;
+                    i++;
+                }
             }
             else {
-                User.Text = comments.comments.DadJokes[0].username;
-                Comment.Text = comments.comments.DadJokes[0].comment;
-                User1.Text = comments.comments.DadJokes[1].username;
-                Comment1.Text = comments.comments.DadJokes[1].comment;
-                User2.Text = comments.comments.DadJokes[2].username;
-                Comment2.Text = comments.comments.DadJokes[2].comment;
-                User3.Text = comments.comments.DadJokes[3].username;
-                Comment3.Text = comments.comments.DadJokes[3].comment;
-                User4.Text = comments.comments.DadJokes[4].username;
-                Comment4.Text = comments.comments.DadJokes[4].comment;
-                User5.Text = comments.comments.DadJokes[5].username;
-                Comment5.Text = comments.comments.DadJokes[5].comment;
-                User6.Text = comments.comments.DadJokes[6].username;
-                Comment6.Text = comments.comments.DadJokes[6].comment;
+                int i = 0;
+                foreach (Label post in comments) {
+                    users[i].Text = commentData.comments.DadJokes[i].username;
+                    comments[i].Text = commentData.comments.DadJokes[i].comment;
+                    i++;
+                }
             }
         }
     }
--- COS368FinalJokeApp/COS368FinalJokeApp/Views/SearchResultPage.xaml.cs
+++ COS368FinalJokeApp/COS368FinalJokeApp/Views/SearchResultPage.xaml.cs
@@ -22,7 +22,40 @@ namespace COS368FinalJokeApp
         {
             base.OnAppearing();
 
-            searchbar.Focus();
+            SearchBar.Focus();
         }
+    private void OnUnfocused(System.Object sender, FocusEventArgs e) {
+           Label.Text = SearchBar.Text;
+           
+           SearchResults searchResults = new SearchResults();
+           Label[] labels = new Label[] { Label1, Label2, Label3, Label3, Label4, Label5, Label6, Label7,Label8, Label9, Label10, Label11 };
+
+           if (SearchBar.Text.Equals("d", StringComparison.OrdinalIgnoreCase)) {
+               int i = 0;
+               foreach (string result in searchResults.jsonData.D.Values) {
+                   labels[i].Text = result;
+                   i++;
+               }
+           }else if (SearchBar.Text.Equals("da", StringComparison.OrdinalIgnoreCase)) {
+               int i = 0;
+               foreach (string result in searchResults.jsonData.Da.Values) {
+                   labels[i].Text = result;
+                   i++;
+               }
+           }else if (SearchBar.Text.Equals("dad", StringComparison.OrdinalIgnoreCase)) {
+               int i = 0;
+               foreach (string result in searchResults.jsonData.Dad.Values) {
+                   labels[i].Text = result;
+                   i++;
+               }
+           }
+           else {
+               int i = 0;
+               foreach (string result in searchResults.jsonData.Dadjokes.Values) {
+                   labels[i].Text = result;
+                   i++;
+               }
+           }
+    }
     }
 }
